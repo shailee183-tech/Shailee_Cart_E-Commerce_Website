0d5a9b8ce4172419b0db4ee7b490760c933cc2b7
Shailee Singh <shailees1102@gmail.com>
2026-06-03 20:04:21 +0530

    Add files via upload

---
+++ backend/store/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import (
+    Category,
+    Product,
+    UserProfile,
+    Order,
+    OrderItem,
+    Cart,
+    CartItem
+)
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(UserProfile)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(CartItem)
